d89e4efa2b4cce16b44a97e8326ab33043af941f
Victor Kist <victoraugustokist@gmail.com>
2021-04-22 18:55:46 -0300

    projeto final do curso de django

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_recita',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
     }
 }
 
--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    mode_preparo = models.TextField()
+    tempo_preparo = models.IntegerField()
+    rendimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    data_receita = models.DateField(default=datetime.now, blank=True)
